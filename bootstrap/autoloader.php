7c9be9855840d2d05b710c9f752f19f07d4f17d8
Yuriy Stolov <yuriystolov@gmail.com>
2022-11-23 14:07:22 +0500

    Optimization

--- bootstrap/autoloader.php
+++ bootstrap/autoloader.php
@@ -2,22 +2,5 @@
 
 declare(strict_types=1);
 
-use Tischmann\Atlantis\Autoloader;
-
-require_once __DIR__ . "/../core/Autoloader.php";
-
-spl_autoload_register("Tischmann\Atlantis\Autoloader::load");
-
-// Подключаем классы фреймворка
-Autoloader::add('Tischmann\Atlantis', 'core');
-
-// Подключаем базовые классы приложения
-Autoloader::add('App', 'app');
-
-// Подключаем классы базы данных
-Autoloader::add('Tischmann\Atlantis\Migrations', 'database');
-
 // Подключение автозагрузчика зависимостей Composer
-if (file_exists(__DIR__ . '/../vendor/autoloader.php')) {
-    require_once __DIR__ . '/../vendor/autoloader.php';
-}
+require_once __DIR__ . '/../vendor/autoload.php';
