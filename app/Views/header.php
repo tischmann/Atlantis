100cf00d44dfc81e31055db4898b9a3576df42ce
Yuriy Stolov <yuriystolov@gmail.com>
2023-02-21 16:55:37 +0500

    Optimization

--- app/Views/header.php
+++ app/Views/header.php
@@ -1,6 +1,6 @@
 <header class="bg-white dark:bg-gray-800 text-black dark:text-white sticky top-0 z-50">
     <div class="md:container md:mx-auto p-4">
-        <a href="/" class="flex items-center" aria-label="{{env=APP_TITLE}}">
+        <a href="/" class="flex items-center mb-4" aria-label="{{env=APP_TITLE}}">
             <img src="/android-chrome-192x192.png" width="32px" height="32px" alt="{{env=APP_TITLE}}" />
             <div class="uppercase text-[34px] leading-8 font-bold">TLANTIS</div>
         </a>
