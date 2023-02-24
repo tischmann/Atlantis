ea1a9489c1b7e8284a3033e6fba27e4541fb8f09
Yuriy Stolov <yuriystolov@gmail.com>
2023-02-24 08:58:50 +0500

    Optimization

--- app/Views/header.php
+++ app/Views/header.php
@@ -1,7 +1,7 @@
 <header class="bg-white dark:bg-gray-800 sticky top-0 z-50">
     <div class="md:container md:mx-auto p-4">
-        <div class="flex flex-col md:flex-row md:items-center md:justify-between mb-4 gap-4">
-            <div class="flex items-center gap-4">
+        <div class="flex flex-row md:items-center flex-wrap md:justify-between mb-4 gap-4">
+            <div class="order-1 flex-grow flex items-center gap-4">
                 <a href="/{{env=APP_LOCALE}}" class="flex items-center" aria-label="{{env=APP_TITLE}}">
                     <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 400 400" xml:space="preserve" width="32px" height="32px" class="text-sky-600 dark:text-sky-500">
                         <path fill="currentColor" d="M99.9,199.8C45,291.1,0,365.8,0,366s19.1,0.4,42.5,0.2l42.5-0.2l57.4-96c31.5-52.7,57.4-96,57.6-96s71.6,118.1,72,119.4
@@ -23,17 +23,15 @@
                 }
                 ?>
             </div>
-            <div class="flex gap-4 items-center">
-                <form class="relative flex items-center w-full md:w-auto" method="GET">
-                    <input type="search" class="relative m-0 block w-1/3 min-w-0 flex-auto rounded-lg border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-1.5 text-base font-normal text-neutral-700 outline-none transition duration-300 ease-in-out focus:border-primary-600 focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200" placeholder="{{lang=search}}" aria-label="{{lang=search}}" name="search" value="<?= $search ?>" />
-                    <span class="input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-base font-normal text-neutral-700 dark:text-neutral-200">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
-                            <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd" />
-                        </svg>
-                    </span>
-                </form>
-                <?php require __DIR__ . "/lang.php" ?>
-            </div>
+            <form class="order-3 md:order-2 relative flex items-center flex-grow md:flex-grow-0" method="GET">
+                <input type="search" class="relative m-0 block w-full md:w-1/3 min-w-[200px] flex-auto rounded-lg border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 pr-10 py-1.5 text-base font-normal text-neutral-700 outline-none transition duration-300 ease-in-out focus:border-primary-600 focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200" placeholder="{{lang=search}}" aria-label="{{lang=search}}" name="search" value="<?= $search ?>" />
+                <span class="absolute right-0 input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-base font-normal text-neutral-700 dark:text-neutral-200">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
+                        <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd" />
+                    </svg>
+                </span>
+            </form>
+            <div class="flex-grow-0 order-2 md:order-3"><?php require __DIR__ . "/lang.php" ?></div>
         </div>
         <?php include __DIR__ . "/breadcrumbs.php" ?>
     </div>
--- app/Views/lang.php
+++ app/Views/lang.php
@@ -1,7 +1,7 @@
 <div class="relative" data-te-dropdown-ref>
-    <button class="flex items-center whitespace-nowrap rounded-lg bg-sky-800 px-3 py-2 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:bg-sky-700 hover:shadow-lg focus:bg-sky-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-sky-700 active:shadow-lg motion-reduce:transition-none" type="button" id="localeSelector" data-te-dropdown-toggle-ref aria-expanded="false" data-te-ripple-init data-te-ripple-color="light">
+    <button class="flex items-center whitespace-nowrap rounded-lg bg-gray-300 dark:bg-sky-800 px-3 py-2 text-xs font-medium uppercase leading-normal transition duration-150 ease-in-out hover:bg-gray-200 focus:bg-gray-200 active:bg-gray-200 dark:hover:bg-sky-700 dark:focus:bg-sky-700 dark:active:bg-sky-700 focus:outline-none focus:ring-0  motion-reduce:transition-none" type="button" id="localeSelector" data-te-dropdown-toggle-ref aria-expanded="false" data-te-ripple-init data-te-ripple-color="light">
         <img src="/images/flags/1x1/{{env=APP_LOCALE}}.svg" width="16" height="16" alt="{{lang=locale_{{env=APP_LOCALE}}}}" />
-        <span class="mx-2 w-2">
+        <span class="pl-2 pr-4 w-2">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
                 <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
             </svg>
@@ -13,7 +13,9 @@
         use Tischmann\Atlantis\Locale;
 
         foreach (Locale::available() as $locale) {
-            $bg = $locale === getenv('APP_LOCALE')
+            $current = $locale === getenv('APP_LOCALE');
+
+            $bg = $current
                 ? 'bg-neutral-100 dark:bg-neutral-600'
                 : 'bg-transparent';
 
@@ -28,6 +30,16 @@
                     <a class="flex items-center gap-3 w-full whitespace-nowrap {$bg} py-2 px-4 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600" href="{$href}" data-te-dropdown-item-ref>
                         <img src="/images/flags/1x1/{$locale}.svg" width="16" height="16" alt="{{lang=locale_{$locale}}}" />
                         <span>{{lang=locale_{$locale}}}</span>
+                HTML;
+
+
+            if ($current) {
+                echo <<<HTML
+                            <i class="fas fa-check pl-3 text-green-500 float-right"></i>
+                        HTML;
+            }
+
+            echo <<<HTML
                     </a>
                 </li>
                 HTML;
