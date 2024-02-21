ee4965c744ee1a8b3ad0c784853645690f231a17
Yuriy Stolov <yuriystolov@gmail.com>
2024-02-21 16:59:13 +0500

    Fix

--- app/Views/header.php
+++ app/Views/header.php
@@ -1,6 +1,6 @@
 <header class="bg-white my-8">
     <div class="md:container mx-8 md:mx-auto">
-        <div class="flex flex-row md:items-center flex-wrap md:justify-between gap-8">
+        <div class="flex flex-row md:items-center flex-wrap md:justify-between gap-4">
             <div class="flex-grow flex items-center gap-4">
                 <a href="/{{env=APP_LOCALE}}" class="flex items-center" aria-label="{{env=APP_TITLE}}">
                     <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 400 400" xml:space="preserve" width="32px" height="32px" class="text-sky-600">
@@ -17,6 +17,16 @@
             use Tischmann\Atlantis\App;
 
             if (App::getCurrentUser()->exists()) {
+                if (App::getCurrentUser()->isAdmin()) {
+                    echo <<<HTML
+                    <a href="/{{env=APP_LOCALE}}/users" aria-label="{{lang=user_list}}" title="{{lang=user_list}}" class="inline-block select-none rounded-xl px-3 py-2 bg-gray-200 hover:bg-gray-300 ">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+                        </svg>
+                    </a>
+                    HTML;
+                }
+
                 echo <<<HTML
                 <a href="/{{env=APP_LOCALE}}/signout" aria-label="{{lang=signout_submit}}" title="{{lang=signout_submit}}" class="flex items-center justify-center h-10 w-10 rounded-xl text-xl bg-gray-200 text-gray-800 transition hover:bg-gray-400 focus:bg-gray-300 focus:outline-none focus:ring-0 active:bg-gray-300 active:shadow-lg">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
--- app/Views/index.php
+++ app/Views/index.php
@@ -1,3 +1,2 @@
 <main class="md:container mx-8 md:mx-auto">
-    <?php include_once 'admin.php'; ?>
 </main>
