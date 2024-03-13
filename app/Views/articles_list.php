5ce7519d3d9ef8659e48e9d883bb2096f47f65a4
Yuriy Stolov <yuriystolov@gmail.com>
2024-03-13 11:05:07 +0500

    Fix

--- app/Views/articles_list.php
+++ app/Views/articles_list.php
@@ -102,4 +102,23 @@ use Tischmann\Atlantis\{Template};
     ?>
     <div class="my-4"><?php include 'pagination.php'; ?></div>
 </main>
-<script src="/js/articles.list.min.js" nonce="{{nonce}}" type="module"></script>
+<script nonce="{{nonce}}" type="module">
+    import Select from '/js/atlantis.select.min.js';
+    [
+        'category_id',
+        'visible',
+        'locale',
+        'fixed',
+        'order',
+        'direction',
+        'moderated'
+    ].forEach((field) => {
+        new Select(document.querySelector(`select[name="${field}"]`), {
+            onchange: (value) => {
+                const url = new URL(window.location.href)
+                url.searchParams.set(field, value)
+                window.location.href = url.toString()
+            }
+        })
+    })
+</script>
