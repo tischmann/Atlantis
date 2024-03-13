c6d85674e2a8f80912e7158b91f1084622834074
Yuriy Stolov <yuriystolov@gmail.com>
2024-03-13 13:57:46 +0500

    Fix

--- app/Views/full_article.php
+++ app/Views/full_article.php
@@ -140,8 +140,8 @@ $image_src = $image
 
             foreach ($article->getAttachements() as $file) {
                 echo <<<HTML
-                <a href="/uploads/articles/{$article->id}/attachements/{$file}" class="grow sm:grow-0 flex flex-nowrap gap-2 w-full sm:w-auto rounded-xl bg-gray-200 p-4 hover:bg-gray-300 hover:underline" download>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <a href="/uploads/articles/{$article->id}/attachements/{$file}" class="grow sm:grow-0 flex items-center flex-nowrap gap-2 w-full sm:w-auto rounded-xl bg-gray-100 dark:bg-gray-700 px-3 py-2 hover:bg-gray-200 dark:hover:bg-gray-600 hover:underline" download>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
                     </svg>
                     {$file}
