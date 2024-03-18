fb640d42892e3700bb08906f7be777d5ec44130e
Yuriy Stolov <yuriystolov@gmail.com>
2024-03-18 10:16:41 +0500

    Fix

--- app/Views/index.php
+++ app/Views/index.php
@@ -1,5 +1,8 @@
+<?php
+$isVisuallyImpaired = cookies_get('vi') === 'true';
+?>
 <main class="md:container mx-4 md:mx-auto mb-4">
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+    <div class="grid grid-cols-1 <?= $isVisuallyImpaired ? "md:grid-cols-2 xl:grid-cols-3" : "md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5" ?> gap-4">
         <?php
 
         foreach ($articles as $article) {
